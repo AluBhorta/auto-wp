0f93ccbd07b1d25f1c1cf26c7eb6aa56f6f75091
alu <dev.bhorta@gmail.com>
2021-12-27 15:21:21 +0600

    add lb resources to pulumi

---
+++ pulumi/lb.ts
@@ -0,0 +1,34 @@
+import * as pulumi from "@pulumi/pulumi";
+import * as aws from "@pulumi/aws";
+
+const auto_wp_alubhorta_com = new aws.acm.Certificate(
+  "auto-wp.alubhorta.com",
+  {
+    tags: {
+      Project: "auto-wp",
+    },
+  },
+  {
+    protect: true,
+  }
+);
+const auto_wp_lb = new aws.alb.LoadBalancer(
+  "auto-wp-lb",
+  {
+    desyncMitigationMode: "defensive",
+    dropInvalidHeaderFields: false,
+    enableCrossZoneLoadBalancing: false,
+    enableDeletionProtection: false,
+    enableHttp2: true,
+    enableWafFailOpen: false,
+    idleTimeout: 60,
+    loadBalancerType: "application",
+    name: "auto-wp-lb",
+    tags: {
+      Project: "auto-wp",
+    },
+  },
+  {
+    protect: true,
+  }
+);
