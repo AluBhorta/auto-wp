ac3edd5d6f3449dcf935cdc580f5f7c7b36fcaf4
alu <dev.bhorta@gmail.com>
2022-01-05 20:58:06 +0600

    fix rds logging issue using both cluster and db parameter groups test creation and logging of cloudwatch log groups update imported json resources

--- pulumi/rds.ts
+++ pulumi/rds.ts
@@ -1,6 +1,57 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
 
+const auto_wp_db_cluster_pg = new aws.rds.ClusterParameterGroup(
+  "auto-wp-db-cluster-pg",
+  {
+    description: "rds aurora mysql cluster parameter group for auto-wp",
+    family: "aurora-mysql5.7",
+    name: "auto-wp-db-cluster-pg",
+    parameters: [
+      {
+        applyMethod: "immediate",
+        name: "general_log",
+        value: "1",
+      },
+      {
+        applyMethod: "immediate",
+        name: "slow_query_log",
+        value: "1",
+      },
+    ],
+  },
+  {
+    protect: true,
+  }
+);
+
+const auto_wp_db_pg = new aws.rds.ParameterGroup(
+  "auto-wp-db-pg",
+  {
+    description: "rds aurora mysql parameter group for auto-wp",
+    family: "aurora-mysql5.7",
+    name: "auto-wp-db-pg",
+    parameters: [
+      {
+        applyMethod: "immediate",
+        name: "slow_query_log",
+        value: "1",
+      },
+      {
+        applyMethod: "immediate",
+        name: "general_log",
+        value: "1",
+      },
+    ],
+    tags: {
+      Project: "auto-wp",
+    },
+  },
+  {
+    protect: true,
+  }
+);
+
 const test_wp_db = new aws.rds.Cluster(
   "test-wp-db",
   {
@@ -11,6 +62,9 @@ const test_wp_db = new aws.rds.Cluster(
     engine: "aurora-mysql",
     engineMode: "provisioned",
     skipFinalSnapshot: true,
+    // allowMajorVersionUpgrade: true,
+    dbClusterParameterGroupName: auto_wp_db_cluster_pg.name,
+    dbInstanceParameterGroupName: auto_wp_db_pg.name,
     enabledCloudwatchLogsExports: ["general", "error", "audit", "slowquery"],
     tags: {
       Project: "auto-wp",
@@ -20,6 +74,7 @@ const test_wp_db = new aws.rds.Cluster(
     protect: true,
   }
 );
+
 const test_wp_db_instance_1 = new aws.rds.ClusterInstance(
   "test-wp-db-instance-1",
   {
@@ -30,7 +85,7 @@ const test_wp_db_instance_1 = new aws.rds.ClusterInstance(
     instanceClass: "db.t2.small",
     monitoringInterval: 60,
     promotionTier: 1,
-
+    dbParameterGroupName: auto_wp_db_pg.name,
     publiclyAccessible: false,
   },
   {
